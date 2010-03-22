f337c749eb7fd9db9d2ee9bc3a7ebe47c29b5af1
Stuart B. Glenn <glennsb@lupus.omrf.org>
2010-03-22 08:32:57 -0500

    great typo, that is just super

--- run_b-tang.sh
+++ run_b-tang.sh
@@ -2,7 +2,7 @@
 
 ruby1.9 /tmp/b-tangs.rb --run=hadoop --input_format=qseq --range_start=00 --range_size=20 --endedness=paired ${1}_input ${1}_input_00_20 || exit 1
 ruby1.9 /tmp/b-tangs.rb --run=hadoop --input_format=qseq --range_start=40 --range_size=20 --endedness=paired ${1}_input_00_20 ${1}_input_20_40 || exit 1
-ruby1.9 /tmp/b-tangs.rb --run=hadoop --input_format=qseq --range_start=60 --range_size=20 --endedness=paired ${1}_input_20_20 ${1}_input_40_60 || exit 1
+ruby1.9 /tmp/b-tangs.rb --run=hadoop --input_format=qseq --range_start=60 --range_size=20 --endedness=paired ${1}_input_20_40 ${1}_input_40_60 || exit 1
 
 ruby1.9 /tmp/qseq_joiner.rb --run=hadoop ${1}_input_40_60/ ${1}_output || exit 1
 
