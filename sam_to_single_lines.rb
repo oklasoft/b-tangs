be6a9ff1db74d86d9b28ac0701d084b1347a2108
Stuart Glenn <glennsb@lupus.omrf.org>
2010-06-21 13:24:25 -0500

    Attempt at sorting based on the strand orientation from the SAM bitwise flags

--- sam_to_single_lines.rb
+++ sam_to_single_lines.rb
@@ -4,6 +4,45 @@ require 'wukong'
 # Given a SAM file, make up something new where the pairs are together one one
 # line. Also we only care about the name, sequence, quality, bit flags, and maps
 module SamToSingleLines
+  IS_REVERSED_STRAND = 16
+  IS_FIRST_READ = 64
+  IS_SECOND_READ = 128
+  
+  # sort them based on the bit mask
+  # the one that the reverse strand will be sorted lower
+  # if they are both reverse or both forward, the second read in the pair is
+  # lower
+  # [83,163].sort {|a,b| SamToSingleLines.reverse_bit_after_forward(a,b)} >> 163,83
+  # [147,99].sort {|a,b| SamToSingleLines.reverse_bit_after_forward(a,b)} >> 99,147
+  # [163,99].sort {|a,b| SamToSingleLines.reverse_bit_after_forward(a,b)} >> 99,163
+  def self.reverse_bit_after_forward(a,b)
+    a_is_reversed = is_reversed?(a)
+    b_is_reversed = is_reversed?(b)
+    
+    return 1 if a_is_reversed && ! b_is_reversed
+    return -1 if b_is_reversed && ! a_is_reversed
+    
+    return -1 if is_first_read(a) && is_second_read(b)
+    return 1 if is_first_read(b) && is_second_read(a)
+    
+    return 0 if a == b
+  end
+  
+  def self.is_reversed?(bit)
+    test_bit(bit,IS_REVERSED_STRAND)
+  end
+  
+  def self.is_first_read(bit)
+    test_bit(bit,IS_FIRST_READ)
+  end
+  
+  def self.is_second_read(bit)
+    test_bit(bit,IS_SECOND_READ)
+  end
+  
+  def self.test_bit(bit,mask)
+    mask == (bit & mask)
+  end
 
   class Mapper < Wukong::Streamer::LineStreamer
 
