67df7dfaa0e037d0ad07e0a18c7ac6b4b2b79123
Stuart Glenn <glennsb@lupus.omrf.org>
2010-08-02 21:49:50 -0500

    Support the same PASS & REJECT in qseq joiner, using .last this time to avoid any problems with positions

--- qseq_joiner.rb
+++ qseq_joiner.rb
@@ -20,6 +20,10 @@ module QseqJoiner
     # values is an array of key, qseq fields
     def finalize
       return unless 2 == values.size
+      
+      return if options[:both_must_pass] && (values[0].last != 'PASS' || values[1].last != 'PASS')
+      return if (values[0].last != 'PASS' && values[1].last != 'PASS')
+      
       values.sort! {|a,b| a[8].to_i <=> b[8].to_i}
       yield [ values[0][1..7], values[0][8..11], values[1][8..11] ]
       # yield [ values[0][8], values[0][1..7], values[0][8..11] ]
