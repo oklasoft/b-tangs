0f676fe24b2a73978c1be22d7dfe27475321da82
Stuart Glenn <glennsb@lupus.omrf.org>
2010-08-03 17:34:51 -0500

    option to allow both to fail

--- flat_fasta_joiner.rb
+++ flat_fasta_joiner.rb
@@ -23,7 +23,7 @@ module FlatFastaJoiner
     def finalize
       return unless 2 == values.size
       return if options[:both_must_pass] && (values[0][6] != 'PASS' || values[1][6] != 'PASS')
-      return if (values[0][6] != 'PASS' && values[1][6] != 'PASS')
+      return if (!options[:allow_both_fail]) && (values[0][6] != 'PASS' && values[1][6] != 'PASS')
 
       values.sort! {|a,b| a[5].to_i <=> b[5].to_i}
       yield [ values[0] ]
