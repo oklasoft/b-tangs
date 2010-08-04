9914bbc83031c69e897613b137765551b4ee256a
Stuart Glenn <glennsb@lupus.omrf.org>
2010-08-04 07:44:58 -0500

    Added the allow_both_fails and single_line options to the qseq joiner

--- qseq_joiner.rb
+++ qseq_joiner.rb
@@ -22,10 +22,15 @@ module QseqJoiner
       return unless 2 == values.size
       
       return if options[:both_must_pass] && (values[0].last != 'PASS' || values[1].last != 'PASS')
-      return if (values[0].last != 'PASS' && values[1].last != 'PASS')
+      return if (!options[:allow_both_fail]) && (values[0].last != 'PASS' && values[1].last != 'PASS')
       
       values.sort! {|a,b| a[8].to_i <=> b[8].to_i}
-      yield [ values[0][1..7], values[0][8..11], values[1][8..11] ]
+      if options[:single_line]
+        values.each {|v| v.shift}
+        yield [ values ]
+      else
+        yield [ values[0][1..7], values[0][8..11], values[1][8..11] ]
+      end
       # yield [ values[0][8], values[0][1..7], values[0][8..11] ]
       # yield [ values[1][8], values[0][1..7], values[1][8..11] ]
     end #finalize
