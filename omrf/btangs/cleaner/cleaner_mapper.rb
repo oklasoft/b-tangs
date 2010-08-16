850ec30c7fd6ad2551cbbbfffaafda3c0ab04b4c
Stuart Glenn <glennsb@lupus.omrf.org>
2010-08-16 12:18:52 -0500

    removing some failed key experiments

--- omrf/btangs/cleaner/cleaner_mapper.rb
+++ omrf/btangs/cleaner/cleaner_mapper.rb
@@ -15,46 +15,10 @@ module Cleaner
       parse_endness_key()
     end
     
-    #
-    # lzop -dc 101292s_1_1_export.txt.lzo| awk -F '\t' '{print $1":"NR"\t"$9"\t"$10}' > 1.txt
-    # lzop -dc lgs101435_s_1_1_qseq_raw.txt.lzo | egrep '1$' | awk -F '\t' '{print $1":"$2":"$3":"$4":"$5":"$6":"$7":"$8"\t"$9"\t"$10}' > 1.txt
-    # awk -F '\t' '2==$8 {print $0}' > 2.txt # for final output
-    #
-    def process line
-      parts = line.chomp.split(/\t/)
-
-      key = line_key(parts)
-      return unless key
-      if key =~ /_possiblepcr/
-        trim_ends_by_resetting_quality_score!(parts) if options[:trim_pcr_quality]
-        trim_ends_by_resetting_reading!(parts) if options[:trim_pcr_read]
-      end
-      if key.kind_of?(Array)
-        key.each do |k|
-          yield [k, *parts]
-        end
-      else
-        yield [key, *parts]
-      end
-    end
-    
-    def trim_ends_by_resetting_quality_score!(parts)
-      new_quality = NO_QUALITY_SCORE * options[:range_size].to_i
-      parts[@quality_col][@key_range] = new_quality
-      (parts[@quality_col].reverse!)[@key_range] = new_quality
-      parts[@quality_col].reverse!
-    end
-
-    def trim_ends_by_resetting_reading!(parts)
-      new_quality = NO_READ * options[:range_size].to_i
-      parts[@sequence_index][@key_range] = new_quality
-      (parts[@sequence_index].reverse!)[@key_range] = new_quality
-      parts[@sequence_index].reverse!
-    end
-    
-    def sequence_index
-      @sequence_index ||= parse_format(options[:input_format]) or
-        raise "Please let us know the input file format with --input_format= argument"
+    # given a start position and a length create a valid range
+    def parse_key_range(start,length)
+      return nil unless options[:range_start] && options[:range_size]
+      Range.new(start.to_i, start.to_i+length.to_i,true)
     end
     
     def parse_format(input_format)
@@ -95,29 +59,32 @@ module Cleaner
         raise "Please supply both a --range_start= and --range_size= argument"
     end
     
-    def acgt_averages(seq)
-      sums = {"A" => 0,"C" => 0,"G" => 0,"T" => 0}
-      seq.chars.each {|c| sums[c] += 1 if sums.keys.include?(c)}
-      sums.each {|l,i| sums[l] = (i/seq.length.to_f*100).floor}
-      sums
-    end
-
-    def compress(term)
-      Zlib::Deflate.deflate(term.strip.downcase)
-    end
-
-    def zlib_key(term)
-      n = "N" * term.size
-      key = (compress(n+term).size - compress(n).size)/term.size.to_f
-      (key * 100).to_i
-    end
-
-    def single_end_key(parts)
-      parts[@sequence_index][@key_range]
+    def sequence_index
+      @sequence_index ||= parse_format(options[:input_format]) or
+        raise "Please let us know the input file format with --input_format= argument"
     end
     
-    def single_end_zlib_key(parts)
-      zlib_key(parts[@sequence_index][@key_range])
+    
+    #
+    # For each line make the key(s) and emit them then for that line
+    #
+    def process(line)
+      parts = line.chomp.split(/\t/)
+
+      key = line_key(parts)
+      return unless key
+
+      if key.kind_of?(Array)
+        key.each do |k|
+          yield [k, *parts]
+        end
+      else
+        yield [key, *parts]
+      end
+    end #process
+    
+    def single_end_key(parts)
+      parts[@sequence_index][@key_range]
     end
     
     def single_end_both_key(parts)
@@ -125,27 +92,7 @@ module Cleaner
       front = sequence[@key_range]
       back = (sequence.reverse)[@key_range].reverse
       key = "#{front}_#{back}"
-      return "#{key}_possiblepcr" if front == back && options[:possible_pcr]
       key
-    end
-    
-    def acgt_key(parts)
-      sequence = parts[@sequence_index]
-      front = sequence[@key_range]
-      avgs = acgt_averages(front)
-      key = []
-      %w/A C G T/.each {|b| key << "#{b}:#{avgs[b]}"}
-      key.join("_")
-    end
-
-    def acgt_both_ends_key(parts)
-      sequence = parts[@sequence_index]
-      front = sequence[@key_range]
-      back = (sequence.reverse)[@key_range].reverse
-      avgs = acgt_averages(front+back)
-      key = []
-      %w/A C G T/.each {|b| key << "#{b}:#{avgs[b]}"}
-      key.join("_")
     end
     
     def paired_end_key(parts)
@@ -179,12 +126,6 @@ module Cleaner
     
     def parse_endness_key
       case options[:key_type]
-        when /acgt_avg/i
-          if options[:both_ends] then
-            alias line_key acgt_both_ends_key
-          else
-            alias line_key acgt_key
-          end
         when /paired/i
           read_end_index()
           if options[:both_ends] then
@@ -212,13 +153,6 @@ module Cleaner
           else
             alias line_key single_end_key
           end
-        when /zlib/i
-          require 'zlib'
-          if options[:both_ends] then
-            alias line_key single_end_zlib_both_key
-          else
-            alias line_key single_end_zlib_key
-          end
         else
           raise "Please specify type of key --key_type (paired, single, acgt_avg)"
       end
@@ -226,11 +160,6 @@ module Cleaner
         raise "Please supply both a --range_start= and --range_size= argument"
     end
     
-    # given a start position and a length create a valid range
-    def parse_key_range(start,length)
-      return nil unless options[:range_start] && options[:range_size]
-      Range.new(start.to_i, start.to_i+length.to_i,true)
-    end
   end #CleanerMapper
 end #cleaner
 end #Btangs
