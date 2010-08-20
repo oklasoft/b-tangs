4f0f50db2365036050dc4fdd0dc586e36088f6de
Stuart Glenn <glennsb@lupus.omrf.org>
2010-08-20 07:13:37 -0500

    Initial line counting wrong for single end

--- clean_sample.rb
+++ clean_sample.rb
@@ -270,7 +270,7 @@ class SampleCleanerApp
     @options.input_files.each do |input_file|
       lines << count_lines(input_file)
     end
-    if lines[0] != lines[1]
+    if 1 != lines.uniq
       return "different number of lines in '#{@options.input_files.join(",")}', #{lines.join(":")}"
     elsif 0 == lines[0] 
       return "No lines in input files '#{@options.input_files.join(",")}"
