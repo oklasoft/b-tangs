3295f38a8621206c53f2403aed623550b1b9f966
Stuart Glenn <Stuart-Glenn@omrf.org>
2012-03-23 15:39:03 -0500

    Adds the casava 1.8 style format

--- omrf/btangs/cleaner/cleaner_reducer.rb
+++ omrf/btangs/cleaner/cleaner_reducer.rb
@@ -53,6 +53,8 @@ module Cleaner
     def name_for(read)
       if INPUT_FORMATS[:fastq] == options[:input_format] || INPUT_FORMATS[:joined_fastq] == options[:input_format] 
         read[0].split(/\//).first #strip of the end /read_number
+      elsif INPUT_FORMATS[:joined_fastq18] == options[:input_format] 
+        read[0].split(/\s/).first #strip of the end /read_number
       elsif INPUT_FORMATS[:qseq] == options[:input_format] || INPUT_FORMATS[:joined_qseq] == options[:input_format]
         read[0..6].join("_")
       end
